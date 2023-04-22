c400042a9886ea29402133aa2888061cc95ef740
bluhhar <bluhhar@DESKTOP-J8AJ9KC>
2023-04-22 22:11:08 +0300

    +create_hidden_repo

--- Passless.Konsole/Program.cs
+++ Passless.Konsole/Program.cs
@@ -19,12 +19,10 @@ namespace Passless.Konsole
         {
             GpgNet.Initialise(@"C:\Program Files (x86)\GnuPG\bin\libgpgme-11.dll");
             Console.WriteLine($"Started GpgME version {GpgNet.Version}");
-            //Test0();
-            //Test1();
-            //Test2();
 
+            //TestCreateRepo();
             //TestAddPassword();
-            TestGetPassword();
+            //TestGetPassword();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
@@ -32,7 +30,14 @@ namespace Passless.Konsole
 
         public static void TestCreateRepo()
         {
-
+            Console.WriteLine("RepoName:");
+            string repoName = Console.ReadLine();
+            string path = @"C:\Users\bluhhar\.passless\" + repoName;
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
         }
 
         public static void TestAddPassword()
@@ -48,7 +53,7 @@ namespace Passless.Konsole
             context.KeylistMode = context.KeylistMode | GpgKeylistMode.WithSecret;
 
             // Print GPG keys
-            var key = context.FindKey("bluh@btwow.ru"); //СДЕЛАТЬ ЧТОБЫ если нет ключа ошибку
+            var key = context.FindKey("bluhhar@btwow.ru"); //СДЕЛАТЬ ЧТОБЫ если нет ключа ошибку
             var result = context.Encrypt(MemoryGpgBuffer.CreateFromString(password), key); //ПОПРОБОВАТЬ РЕАЛИЗОВАТЬ ЧТОБЫ вместо keys бралось email при вводе в файл подписи
 
             var encPasswordFile = Environment.ExpandEnvironmentVariables(@"%userprofile%\.passless\" + fileName + ".gpg");
@@ -74,99 +79,6 @@ namespace Passless.Konsole
 
             Console.WriteLine("Decryption output:");
             Console.WriteLine(content);
-        }
-
-        private static void Test2()
-        {
-
-            var helloWorld = "Hello World!";
-            Console.WriteLine(helloWorld);
-
-            var context = GpgContext.CreateContext();
-
-            context.KeylistMode = context.KeylistMode | GpgKeylistMode.WithSecret;
-            context.ArmorMode = GpgArmorMode.On;
-
-            // Print GPG keys
-            var keys = context.FindKeys().ToArray();
-            foreach (var key in keys)
-            {
-                Console.WriteLine(key.ToString());
-                Console.WriteLine($"\t{key.Uids.First()}");
-                foreach (var subkey in key.Subkeys)
-                {
-                    Console.WriteLine($"\t{subkey}");
-                }
-            }
-
-
-            MemoryGpgBuffer helloBuffer = MemoryGpgBuffer.CreateFromString(helloWorld);
-            var encryptedBuffer = context.Encrypt(helloBuffer, keys[1]); //ПОПРОБОВАТЬ РЕАЛИЗОВАТЬ ЧТОБЫ вместо keys бралось email при вводе в файл подписи
-            var encryptedByteBuffer = new byte[(int)encryptedBuffer.Length];
-            encryptedBuffer.Read(encryptedByteBuffer, 0, (int)encryptedBuffer.Length);
-            Console.WriteLine("========");
-            Console.WriteLine(Encoding.UTF8.GetString(encryptedByteBuffer));
-            Console.WriteLine("========");
-            encryptedBuffer.Position = 0;
-            var prop = new byte[2];
-
-            using (var decrypted = context.Decrypt(encryptedBuffer))
-            {
-                var decryptedByteBuffer = new byte[decrypted.Length];
-                encryptedBuffer.Position = 0;
-                var decryptedBuffer = context.Decrypt(encryptedBuffer);
-                decryptedBuffer.Read(decryptedByteBuffer, 0, decryptedByteBuffer.Length);
-                Console.WriteLine(Encoding.UTF8.GetString(decryptedByteBuffer));
-            }
-        }
-
-        private static void Test1()
-        {
-            var context = GpgContext.CreateContext();
-            var recipient = context.FindKey("dev");
-
-            var reencFile = Environment.ExpandEnvironmentVariables(@"%userprofile%\.passless\testpw-reenc.gpg");
-            var file = MemoryGpgBuffer.CreateFromFile(reencFile);
-            var plaintext = new StreamReader(context.Decrypt(file)).ReadToEnd();
-            Console.WriteLine(plaintext);
-        }
-
-        private static void Test0()
-        {
-            // Create a new GPG context
-            var context = GpgContext.CreateContext();
-
-            context.KeylistMode = context.KeylistMode | GpgKeylistMode.WithSecret;
-
-            // Print GPG keys
-            var keys = context.FindKeys().ToArray();
-            foreach (var key in keys)
-            {
-                Console.WriteLine(key.ToString());
-                Console.WriteLine($"\t{key.Uids.First()}");
-                foreach (var subkey in key.Subkeys)
-                {
-                    Console.WriteLine($"\t{subkey}");
-                }
-            }
-
-            var passwordFile = Environment.ExpandEnvironmentVariables(@"%userprofile%\.passless\testpw-reenc.gpg");
-            // Create a GPG data buffer for storing the ciphertext
-            var inputBuffer = MemoryGpgBuffer.CreateFromFile(passwordFile);
-
-            var content = new StreamReader(context.Decrypt(inputBuffer)).ReadToEnd();
-
-            Console.WriteLine("Decryption output:");
-            Console.WriteLine(content);
-
-            var result = context.Encrypt(MemoryGpgBuffer.CreateFromString(content + "Re-Encrypted: True\n"), keys.Take(1));
-
-            var reEncPasswordFile = Environment.ExpandEnvironmentVariables(@"%userprofile%\.passless\testpw-reenc.gpg");
-            using (var fs = File.OpenWrite(reEncPasswordFile))
-            {
-                result.CopyTo(fs);
-            }
-            Console.WriteLine("File was re-encrypted successfully.");
         }
     }
 }
