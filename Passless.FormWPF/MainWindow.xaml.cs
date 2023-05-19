d4f1fe65a2eae77ba927b5ceaef638125055b0b4
bluhhar <bluhhar@DESKTOP-J8AJ9KC>
2023-05-19 17:57:07 +0300

    +get password in wpf

--- Passless.FormWPF/MainWindow.xaml.cs
+++ Passless.FormWPF/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Passless.Classes.Passwords;
+using Passless.Modules;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,17 +24,36 @@ namespace Passless.FormWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _selectedLocationPath = Environment.ExpandEnvironmentVariables(@"%userprofile%\.passless\");
+        private string _keyOwner = null;
+
         private List<string> passwords; // Ваши пароли
+
+        private void LoadPasswords()
+        {
+            passwords = new List<string>();
+            DirectoryInfo dir = new DirectoryInfo(_selectedLocationPath);
+            FileInfo[] files = dir.GetFiles("*.gpg");
+            foreach (var file in files)
+            {
+                passwords.Add(file.Name);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            passwords = new List<string>()
-            {
-                "githubPassword1",
-                "githubPassword2",
-                "otherPassword1",
-                "otherPassword2"
-            };
+            _keyOwner = FileHelper.Reader(_selectedLocationPath);
+            Controller.Activation();
+            //passwords = new List<string>()
+            //{
+            //    "githubPassword1",
+            //    "githubPassword2",
+            //    "otherPassword1",
+            //    "otherPassword2"
+            //};
+            LoadPasswords();
+
             passwordListBox.ItemsSource = passwords;
         }
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -64,8 +87,16 @@ namespace Passless.FormWPF
             string selectedPassword = passwordListBox.SelectedItem as string;
             if (selectedPassword != null)
             {
-                // Добавьте вашу логику обработки выбранного пароля здесь
-                MessageBox.Show("Выбран пароль: " + selectedPassword);
+                string password = GetPassword.GetPasswordFromRepository(_selectedLocationPath + selectedPassword);
+                Clipboard.SetText(password);
+                Thread t = new Thread(() =>
+                {
+                    Thread.Sleep(30000);
+                    Clipboard.Clear();
+                });
+                t.SetApartmentState(ApartmentState.STA);
+                t.Start();
+                //MessageBox.Show("Выбран пароль: " + selectedPassword);
             }
         }
     }
